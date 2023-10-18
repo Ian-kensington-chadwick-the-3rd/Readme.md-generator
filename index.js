a45711bce4c09872ac66cb495b4f894883538311
Ian Sills <iansills04@gmail.com>
2023-10-17 19:35:34 -0500

    added homework initial files

---
+++ index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+const inquire = require('inquirer')
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+
+
+
+
+}
+
+// Function call to initialize app
+init();
+// user inquire to display message to user
+// call generate mark down function on the data collected from user 
+// take what is returned to us then pass it to the writeToFile function
+// research fs and inquire
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,32 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  // create a conditional to check if there is a license
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+// create a conditional to check if there is a license
+
+// create a conditional to check if there is a license
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+// create a conditional to check if there is a license
+
+
+
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  // plug in data from user and put it into the readme file
+  return `# ${data.title}
+ 
+`;
+}
+// create layout of what the readme file will need to print using template literals
+module.exports = generateMarkdown;
