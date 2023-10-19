08030052d14029ecbbce50fe2fa0c2ccd4ff8b1f
Ian Sills <iansills04@gmail.com>
2023-10-19 00:43:28 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
 // TODO: Include packages needed for this application
-const inquire = require('inquirer')
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = 
+// add readme questions (description, usage, license, etc)
+    [{
+        type: 'input',
+        message: 'what is your Github name?',
+        name: 'Github name',
+        validate: (value)=>{ if(value) {return true} else{return 'i need a value to continue'}} 
+    },
+    {
+        type: 'input',
+        message: 'what is your email address?',
+        name: 'Github name',
+        validate: (value)=>{ if(value) {return true} else{return 'i need a value to continue'}} 
+    },
+    {
+        type: 'input',
+        message: 'what is the project title?',
+        name: 'title',
+        validate: (value)=>{ if(value) {return true} else{return 'i need a value to continue'}} 
+    },
+    {
+        type: 'list',
+        message: 'please choose a license!',
+        name: 'license',
+        choices:['MIT', 'BSD-3-Clause', 'Apache', 'GPL'],
+        validate: (value)=>{ if(value) {return true} else{return 'you have to choose a license!!!'}} 
 
+    }
+   
+];
+ 
+  
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {
-
-
+function writeToFile(fileName, generateMarkdown) {
+// fs write file to write to readme.md file
+generateMarkdown(answers);
 
 
 }
+
+// TODO: Create a function to initialize app
+function init() {
+ inquirer
+    .prompt(questions)
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+      writeToFile(readme.md);
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.log('Prompt couldnt be rendered in the current environment')
+      } else {
+        // Something else went wrong
+        console.log('Something else went wrong')
+      }
+    })  
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,22 +1,27 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+// must have a couple license options
 function renderLicenseBadge(license) {
   // create a conditional to check if there is a license
+ 
+
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
 // create a conditional to check if there is a license
+// returns a string that is a link
 
-// create a conditional to check if there is a license
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
 // create a conditional to check if there is a license
-
+// call renderlicenseBadge and renderlicenselink
+// use template literal to format license section and call functions
+// return template literal
 
 
 }
@@ -24,9 +29,12 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   // plug in data from user and put it into the readme file
-  return `# ${data.title}
- 
-`;
+  // create layout of what you markdown readme with template literals
+  // hint - call renderlicense functions inside this function
+  return `# ${data.title}`;
+   
+
+
 }
 // create layout of what the readme file will need to print using template literals
 module.exports = generateMarkdown;
