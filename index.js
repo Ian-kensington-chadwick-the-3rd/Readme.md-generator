e8369f68a0977d8cb00675c499c2f8f9a3c54ab3
Ian Sills <iansills04@gmail.com>
2023-10-19 22:37:58 -0500

    updated generateMarkdown

--- index.js
+++ index.js
@@ -1,55 +1,85 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
-const generateMarkdown = require('./utils/generateMarkdown')
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
 // TODO: Create an array of questions for user input
 const questions = 
 // add readme questions (description, usage, license, etc)
     [{
         type: 'input',
         message: 'what is your Github name?',
-        name: 'Github name',
-        validate: (value)=>{ if(value) {return true} else{return 'i need a value to continue'}} 
+        name: 'Githubname',
+        validate: (data)=>{ if(data) {return true} else{return 'i need a value to continue'}} 
     },
     {
         type: 'input',
         message: 'what is your email address?',
-        name: 'Github name',
-        validate: (value)=>{ if(value) {return true} else{return 'i need a value to continue'}} 
+        name: 'email',
+        validate: (data)=>{ if(data) {return true} else{return 'i need a value to continue'}} 
     },
     {
         type: 'input',
         message: 'what is the project title?',
         name: 'title',
-        validate: (value)=>{ if(value) {return true} else{return 'i need a value to continue'}} 
+        validate: (data)=>{ if(data) {return true} else{return 'i need a value to continue'}} 
     },
     {
         type: 'list',
         message: 'please choose a license!',
         name: 'license',
         choices:['MIT', 'BSD-3-Clause', 'Apache', 'GPL'],
-        validate: (value)=>{ if(value) {return true} else{return 'you have to choose a license!!!'}} 
+        validate: (data)=>{ if(data) {return true} else{return 'you have to choose a license!!!'}} 
 
-    }
+    },
+    {
+      type: 'input',
+      message: 'Contributers? If there are none then press enter',
+      name: 'contributers',
+    },
+    {
+      type: 'input',
+      message: 'how would I go about using your project? *usage',
+      name: 'usage',
+      validate: (data)=>{ if(data) {return true} else{return 'i need a value to continue'}} 
+    },
+    {
+      type: 'input',
+      message: 'how do you install your project?',
+      name: 'installation',
+      validate: (data)=>{ if(data) {return true} else{return 'i need a value to continue'}} 
+    },
+
    
 ];
  
   
-// TODO: Create a function to write README file
-function writeToFile(fileName, generateMarkdown) {
-// fs write file to write to readme.md file
-generateMarkdown(answers);
+//  TODO: Create a function to write README file
+//  fs write file to write to readme.md file
+ function writeToFile(data) {
+  
+   fs.writeFile('newreadme.md', generateMarkdown(data), (err) => {
+     if (err) throw err;
+       console.log('The file has been saved!');
+     
+    });
 
+  
 
-}
+ }
 
 // TODO: Create a function to initialize app
 function init() {
  inquirer
     .prompt(questions)
-    .then((answers) => {
+    .then((data) => {  
+      console.log(data)
+      writeToFile(data);
+    
+    
       // Use user feedback for... whatever!!
-      writeToFile(readme.md);
+      
+      
     })
     .catch((error) => {
       if (error.isTtyError) {
---
+++ newreadme.md
@@ -0,0 +1,21 @@
+[Project Title] # Project Title
+kdkd
+## Insallation
+
+dkkdkd
+
+## Contributers
+
+undefined
+
+## License
+
+GPL
+
+## Github name
+
+ian
+
+## email  
+
+turtle
